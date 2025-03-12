b1ad04653dba839fcad0b06d885d4b57e63ffe18
kelvin Githinji <githinjikelvin74@gmail.com>
2025-03-12 15:13:50 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+  console.log("After destructivelyAppendCat:", cats);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+  console.log("After destructivelyPrependCat:", cats);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  console.log("After destructivelyRemoveLastCat:", cats);
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  console.log("After destructivelyRemoveFirstCat:", cats);
+}
+
+
+function appendCat(name) {
+  const newCats = [...cats, name];
+  console.log("After appendCat:", newCats);
+  return newCats;
+}
+
+function prependCat(name) {
+  const newCats = [name, ...cats];
+  console.log("After prependCat:", newCats);
+  return newCats;
+}
+
+function removeLastCat() {
+  const newCats = cats.slice(0, -1);
+  console.log("After removeLastCat:", newCats);
+  return newCats;
+}
+
+function removeFirstCat() {
+  const newCats = cats.slice(1);
+  console.log("After removeFirstCat:", newCats);
+  return newCats;
+}
+
+console.log("Initial cats array:", cats);
+destructivelyAppendCat("Ralph");
+destructivelyPrependCat("Bob");
+destructivelyRemoveLastCat();
+destructivelyRemoveFirstCat();
+appendCat("Broom");
+prependCat("Arnold");
+removeLastCat();
+removeFirstCat();
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+
+ 
+
